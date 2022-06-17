9b5cc3991058a3c0b92907fbac2452c16cee204b
med amine <aminelife86@gmail.com>
2022-06-17 16:32:43 +0200

    fisre save

---
+++ src/components/Posts.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import MyImage from "../MyImage.jpg"
+
+
+const Posts = () => {
+
+  
+  
+  return (
+    <div>
+      <h1>Liste des postes</h1>
+     
+      <img src={MyImage} alt="img" />
+  
+    </div>
+  )
+}
+
+export default Posts
